a94b7b91145bdb8b3005c7132710f6cb36c6b18e
your new username <smallmonster900@gmail.com>
2023-11-29 09:29:33 +0700

    adasd

--- src/main/java/com/ra/config/AppConfig.java
+++ src/main/java/com/ra/config/AppConfig.java
@@ -1,11 +1,10 @@
 package com.ra.config;
 
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.*;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -19,7 +18,11 @@ import org.thymeleaf.templatemode.TemplateMode;
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.ra")
+@PropertySource("classpath:config.properties")
 public class AppConfig implements WebMvcConfigurer, ApplicationContextAware {
+
+    @Value("${path}")
+    private String pathUpload;
     private ApplicationContext applicationContext;
 
     @Bean
@@ -60,8 +63,8 @@ public class AppConfig implements WebMvcConfigurer, ApplicationContextAware {
     }
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/css/**","/fonts/**","/images/**","/js/**")
-                .addResourceLocations("classpath:assets/css/","classpath:assets/fonts/","classpath:assets/images/",
+        registry.addResourceHandler("/css/**","/fonts/**","/images/**","/js/**","/uploads/**")
+                .addResourceLocations("file:"+pathUpload,"classpath:assets/css/","classpath:assets/fonts/","classpath:assets/images/",
                         "classpath:assets/js/");
     }
 
